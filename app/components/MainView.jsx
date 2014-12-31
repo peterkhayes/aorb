efd25055884c7040eaa850421f34e206a0b0e4d0
Peter Hayes <peter.k.hayes@gmail.com>
2014-12-30 23:54:44 -0800

    refactor to double firebase lookup

--- app/components/MainView.jsx
+++ app/components/MainView.jsx
@@ -1,24 +1,44 @@
 var React = require("react");
 var Firebase = require("../factories/Firebase");
+var FriendView = require('./FriendView.jsx');
+var async = require("async");
 
 var MainView = React.createClass({
   
   getInitialState: function() {
     return {
-      decisions: []
+      decisions: [],
+      currentDecision: null,
+      viewFriends: false 
     }
   },
 
   componentWillMount: function() {
     var that = this;
-    that._userDecisionsRef = new Firebase("users", that.props.username, "decisions");
-    that._userDecisionsRef.on("child_added", function(snap) {
-      new Firebase("decisions", snap.key()).once("value", function(snap) {
-        var val = snap.val();
-        var newDecisions = (that.state.decisions).concat(val);
-        that.setState({decisions: newDecisions});
+    that._userRef = new Firebase("users", that.props.username);
+    that._userRef.on("value", function(snap) {
+      var val = snap.val();
+      var decisions = Object.keys(val.decisions);
+      var friends = Object.keys(val.friends);
+      async.map(decisions, function(decision, cb) {
+        new Firebase("decisions", decision).once("value", function(snap) {
+          return cb(null, snap.val());
+        });
+      }, function(err, decisions) {
+        that.setState({
+          decisions: decisions,
+          friends: friends
+        });
       });
     });
+    // that._userRef.child("decisions").on("child_added", function(snap) {
+    //   new Firebase("decisions", snap.key()).once("value", function(snap) {
+    //     var val = snap.val();
+    //     var newDecisions = (that.state.decisions).concat(val);
+    //     that.setState({decisions: newDecisions});
+    //   });
+    // });
+    // that._userRef.child("friends")
   },
 
   componentWillUnmount: function() {
@@ -29,6 +49,11 @@ var MainView = React.createClass({
     var decisions = this.state.decisions.map(function(decision) {
       return <li>{decision.name}</li>
     });
+
+    if (this.state.viewFriends) {
+      return (<FriendView username={this.props.username} addFriend={this._addFriend}/>)
+    }
+
     return (
       <div>
         <button>New Decision</button>
@@ -39,7 +64,9 @@ var MainView = React.createClass({
         </ul>
       </div>
     );
-  }
+  },
+
+
 })
 
 module.exports = MainView;
